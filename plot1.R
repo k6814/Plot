66b6b8154aaf61689eeea093a4873f8f441c3e8e
k6814 <kevingarda7@gmail.com>
2016-09-07 00:28:24 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,3 @@
+hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
